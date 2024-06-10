0f2c21744d8016f027558c6501c4e6efd97f2995
techniadrian <111449883+techniadrian@users.noreply.github.com>
2024-06-10 10:51:27 +0200

    Add some logic in ParticipantModal

--- MessengerTS/src/components/ParticipantModal/ParticipantModal.tsx
+++ MessengerTS/src/components/ParticipantModal/ParticipantModal.tsx
@@ -4,9 +4,15 @@ import styles from "./styles";
 import { ParticipantModalProps } from "./types";
 import { useMutation, useQuery } from "@tanstack/react-query";
 import api from "../../api";
+import { RouteProp, useRoute } from "@react-navigation/native";
+import { ParamList } from "../../../App";
+import { Participant } from "../../types";
 
 const ParticipantModal: React.FC<ParticipantModalProps> = (props) => {
   const {} = props;
+
+  const { params } = useRoute<RouteProp<ParamList, "Conversation">>();
+  const { conversationId } = params;
 
   const { data: users } = useQuery({
     queryKey: ["api.users.list"],
@@ -14,13 +20,20 @@ const ParticipantModal: React.FC<ParticipantModalProps> = (props) => {
   });
 
   const { data: participants } = useQuery({
-    queryKey: ["api.conversations.participants.list"],
-    queryFn: () => api.conversations.participants.list(),
+    queryKey: ["api.conversations.participants.list", conversationId],
+    queryFn: () => api.conversations.participants.list(conversationId),
   });
 
   const { mutate: createParticipant } = useMutation({
-    mutationKey: ["api.conversations.participants.create"],
-    mutationFn: () => api.conversations.participants.create(),
+    mutationKey: ["api.conversations.participants.create", conversationId],
+    mutationFn: (variables: Omit<Participant, "_id">) =>
+      api.conversations.participants.create(conversationId, variables),
+  });
+
+  const { mutate: deleteParticipant } = useMutation({
+    mutationKey: ["api.conversations.participants.delete", conversationId],
+    mutationFn: (variables: Participant["_id"]) =>
+      api.conversations.participants.delete(conversationId, variables),
   });
 
   return <View style={[styles.container]}></View>;
